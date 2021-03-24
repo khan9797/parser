b98873f2e5ab0f3155144d47044bafd97edcd0cc
Basit Ali <basit@sahilsemi.com>
2021-03-24 13:25:25 +0500

    Update checksum l4 calculation for the packet created

--- checksum.py
+++ checksum.py
@@ -4,21 +4,24 @@ payload = '0'*640
 
 # Get the TCP checksum computed by Scapy
 packet = IP()/TCP()/payload
-packet = IP(raw(packet))  # Build packet (automatically done when sending)
+# packet = IP(raw(packet))  # Build packet (automatically done when sending)
+packet = packet.__class__(bytes(packet))
 # print(packet.show())
 checksum_scapy = packet[TCP].chksum
 print(hex(checksum_scapy))
 
 # Get the UDP checksum computed by Scapy
 packet = IP()/UDP()/payload
-packet = IP(raw(packet))  # Build packet (automatically done when sending)
+# packet = IP(raw(packet))  # Build packet (automatically done when sending)
+packet = packet.__class__(bytes(packet))
 # print(packet.show())
 checksum_scapy = packet[UDP].chksum
 print(hex(checksum_scapy))
 
 # Get the SCTP checksum computed by Scapy
 packet = IP()/SCTP()/payload
-packet = IP(raw(packet))  # Build packet (automatically done when sending)
+# packet = IP(raw(packet))  # Build packet (automatically done when sending)
+packet = packet.__class__(bytes(packet))
 # print(packet.show())
 checksum_scapy = packet[SCTP].chksum
 print(hex(checksum_scapy))
